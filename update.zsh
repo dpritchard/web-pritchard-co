a4aa063fb0ec60c9f9b01c8e89b002c73dce7e48
Daniel Pritchard <daniel@pritchard.co>
2022-11-18 07:34:20 +1300

    Updates

--- update.zsh
+++ update.zsh
@@ -2,6 +2,6 @@
 composer update
 npm update
 ./vendor/bin/jigsaw build production
-npm run production
+npm run prod
 git add build_production && git commit -m "Build for deploy"
 git subtree push --prefix build_production origin gh-pages
